01a77ca97c838e32b06d185cb9ac701ee1c9eb17
Soneka96 <goncalorenhafernandes@gmail.com>
2026-09-24 15:01:03 +0100

    refactor(pairing): remove redundant handshake mapping

--- app/lib/features/pairing/data/repositories/pairing.repository.dart
+++ app/lib/features/pairing/data/repositories/pairing.repository.dart
@@ -1,7 +1,6 @@
 import 'package:fpdart/fpdart.dart';
 
 import 'package:dovahlink_client/features/pairing/data/datasources/pairing_remote.datasource.dart';
-import 'package:dovahlink_client/features/pairing/data/models/pairing_handshake.model.dart';
 import 'package:dovahlink_client/features/pairing/domain/entities/pairing_handshake.entity.dart';
 import 'package:dovahlink_client/features/pairing/domain/repositories/pairing_repository.dart';
 import 'package:dovahlink_client/shared/constants/enums.dart';
@@ -17,10 +16,8 @@ class PairingRepository implements IPairingRepository {
 
   /// See [IPairingRepository.authenticate].
   @override
-  Future<Either<Failure, PairingHandshake>> authenticate() async =>
-      (await _remoteDataSource.authenticate()).map(
-        (PairingHandshakeModel model) => model,
-      );
+  Future<Either<Failure, PairingHandshake>> authenticate() =>
+      _remoteDataSource.authenticate();
 
   /// See [IPairingRepository.requestPairingCode].
   @override
